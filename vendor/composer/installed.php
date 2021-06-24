b16184e136be8905cafe2b2cf69efea588ffee13
jeherve <jeherve@users.noreply.github.com>
2021-06-24 11:13:13 +0000

    Merge pull request #20098 from Automattic/add/vaultpress-plugin-monorepo
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/967694539

---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'automattic/vaultpress',
+  ),
+  'versions' => 
+  array (
+    'automattic/jetpack-autoloader' => 
+    array (
+      'pretty_version' => '2.10.x-dev',
+      'version' => '2.10.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'eaafab74b82f52598161008c72d19d6b005b999e',
+    ),
+    'automattic/jetpack-logo' => 
+    array (
+      'pretty_version' => '1.5.x-dev',
+      'version' => '1.5.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bc91bf58cf7ffa4a606336e07917d3250565a1c5',
+    ),
+    'automattic/vaultpress' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
