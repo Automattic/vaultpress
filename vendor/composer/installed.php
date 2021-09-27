49ebf76f8ef2a83a0be6fa4bf91d8f509e34dc26
anomiex <anomiex@users.noreply.github.com>
2021-09-27 20:31:56 +0000

    Update composer requirement to 2.1 (#21202)
    PHP 8.1 is coming soon, and it makes Composer 2.0 spit out tons of
    deprecation warnings. Plus 2.1 has been out for a while now. Seems like
    a good time to finally update.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1279869434

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,42 +1,41 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-master',
-    'version' => 'dev-master',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'automattic/vaultpress',
+        'dev' => false,
     ),
-    'reference' => NULL,
-    'name' => 'automattic/vaultpress',
-  ),
-  'versions' => 
-  array (
-    'automattic/jetpack-autoloader' => 
-    array (
-      'pretty_version' => '2.10.x-dev',
-      'version' => '2.10.9999999.9999999-dev',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '0223d7872c1abcfac860e4bb4e10c8e2b1ca405d',
+    'versions' => array(
+        'automattic/jetpack-autoloader' => array(
+            'pretty_version' => '2.10.x-dev',
+            'version' => '2.10.9999999.9999999-dev',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
+            'aliases' => array(),
+            'reference' => '0223d7872c1abcfac860e4bb4e10c8e2b1ca405d',
+            'dev_requirement' => false,
+        ),
+        'automattic/jetpack-logo' => array(
+            'pretty_version' => '1.5.x-dev',
+            'version' => '1.5.9999999.9999999-dev',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../automattic/jetpack-logo',
+            'aliases' => array(),
+            'reference' => '03033fb3227607f17de8a71edf9307e98dc4547b',
+            'dev_requirement' => false,
+        ),
+        'automattic/vaultpress' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
     ),
-    'automattic/jetpack-logo' => 
-    array (
-      'pretty_version' => '1.5.x-dev',
-      'version' => '1.5.9999999.9999999-dev',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '03033fb3227607f17de8a71edf9307e98dc4547b',
-    ),
-    'automattic/vaultpress' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-      ),
-      'reference' => NULL,
-    ),
-  ),
 );
