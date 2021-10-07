73a99652fd846de5be37bb7ec7028822cde23cd1
miguelxpn <miguelxpn@users.noreply.github.com>
2021-10-07 16:19:38 +0000

    Updated package versions for automattic/vaultpress
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1316978523

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -11,21 +11,21 @@
     ),
     'versions' => array(
         'automattic/jetpack-autoloader' => array(
-            'pretty_version' => '2.10.x-dev',
-            'version' => '2.10.9999999.9999999-dev',
+            'pretty_version' => 'v2.10.7',
+            'version' => '2.10.7.0',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'ce977a5c12235e70345b5f4fb516cc06a9e215d8',
+            'reference' => 'e3dfa1a2679e4b8e380bc37b722081875c84a381',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
-            'pretty_version' => '1.5.x-dev',
-            'version' => '1.5.9999999.9999999-dev',
+            'pretty_version' => 'v1.5.8',
+            'version' => '1.5.8.0',
             'type' => 'library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => '6314a1c6f474bc6c0d58337875c9c3b95f6291bc',
+            'reference' => 'c3045c4aa008507a13ee02ae5035dc91b3ff3ae4',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
