3fa72d56d0cee9f698aa87b56e62c578a37ef216
anomiex <anomiex@users.noreply.github.com>
2021-12-14 15:07:27 +0000

    Add i18n-check-webpack-plugin project (#22078)
    This Webpack plugin will compare the i18n present in the source
    resources versus the output assets to detect when i18n has been mangled
    by Webpack optimization. It also checks for a few other i18n errors in
    the output assets.
    
    This also fixes a lot of i18n errors detected by the new plugin.
    For the most part that's minifier breakage where code like
    ```js
    flag ? __( 'foo', 'domain' ) : __( 'bar', 'domain' )
    ```
    gets transformed into
    ```js
    __( flag ? 'foo' : 'bar', 'domain' )
    ```
    which breaks the detection of the two strings as needing translation.
    It also cleans up a few places where the domain was omitted, and adds
    some context to a few translations.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1578412316

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'e1c128b710471c0cda082cce6bdff10c165fd030',
+            'reference' => '7cd7be53417c8d42fd73332d947af408c2dd25e8',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => '58ff18cbaf397c4253f9919c945a985de90dfc67',
+            'reference' => '9317f613b0fbb92b653665628fc85a615c8c26ec',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
