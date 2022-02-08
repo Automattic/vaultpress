fe9b1de0af7abb87e8c05474c54327bb93ea66d3
jeherve <jeherve@users.noreply.github.com>
2022-02-08 14:58:10 +0000

    Adding working link and explanation for Jetpack unit tests (#22735)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1812908746

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'ad706fa12bd638924b0e3d3cc18ab0a05a9c889e',
+            'reference' => '0c921bb57c0d83d8cc980da062db0bd66900204a',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => '36cda46fa870d2608b6dbf5bc8265283d6a3833a',
+            'reference' => '7740d631b6095b9f3cca6c75ee483d051e5adb56',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
