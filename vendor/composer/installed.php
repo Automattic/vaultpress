1f3dbb63aceb1d290fc8ff9f699b8bc4eff1303c
jeherve <jeherve@users.noreply.github.com>
2022-02-08 08:49:09 +0000

    Allow plugins that consume the Sync package to configure the data settings (#22458)
    Co-authored-by: Igor Zinovyev <zinigor@gmail.com>
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1811173668

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '2ab5bfbbb03711a9b05378af240d291faa7d40d7',
+            'reference' => 'e0870e014b255a24555780a4ac14606a05e75f8d',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => '0782ae285e2e4d70139e0f7f5911c4befe7ee0d8',
+            'reference' => '9258b7e209a136b990080a89e5e0b549c0a97e8c',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
