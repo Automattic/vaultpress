8278ec2552d0afbe5a8d3853a8fb3a7c47c61d1d
anomiex <anomiex@users.noreply.github.com>
2022-02-11 16:07:15 +0000

    storybook: Try storybook-addon-turbo-build (#22774)
    The main change here (according to its documentation) is that it will
    use esbuild instead of terser for minification. Since we don't need
    things like preserving of translator comments in the storybook, that's
    probably ok. Let's see how it performs.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1830235969

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '64f2f013605d28ad18a9404310056c6bae5b019c',
+            'reference' => '5d3ca5a21221f049e1e6292ae43693a88e2a2066',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => '8e0bb8d1b9649099dddb902b9a038a73264d2ec5',
+            'reference' => '3a5d6a61d1ae83342b7b7a726c6dd6eb1a91ebe9',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
