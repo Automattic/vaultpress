75edc6168770a924797993aa27c81624adcb9838
retrofox <retrofox@users.noreply.github.com>
2022-02-11 22:45:18 +0000

    My Jetpack: Handle `Add Search` link to my Jetpack dashboard card (#22800)
    * [not verified] my-jetpack: update mock data. useMyJetpackNavigate()
    
    * [not verified] changelog
    
    * my-jetpack: remove consts instances when routing
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1831849218

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '6aba3802a2f3d9cf3ecee874e23b3360dbcfbaaa',
+            'reference' => '3c7d3a7265d53a9d648bee249546a228776cc110',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => 'acfde278d20a1f115e8997f295381367623825dc',
+            'reference' => 'f02f255707010937b9f207b56939863ab8a02023',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
