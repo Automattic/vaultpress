c61e75af528b0865b4b3562a45d64501d14493fb
retrofox <retrofox@users.noreply.github.com>
2022-02-15 15:43:45 +0000

    My Jetpack: Do not lead to checkout when has required plan (#22894)
    * [not verified] my-jetpack: do not show product price when has plan
    
    * [not verified] changelog
    
    * my-jetpack: lead to checkout when needs purchase
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1847871996

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '5616021a0dd73edda198d8fb95d4160eb2e30168',
+            'reference' => '96f90726c05ad68789889456b8af7d348ca16a99',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => '0b8fb805e77e078cbdd8ad9315dbd86d60403da6',
+            'reference' => '4aa39244f7617d1c723d483a6dfa86f27f47891f',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
