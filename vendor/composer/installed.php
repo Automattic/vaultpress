cf76ff194f21e5721f969b35445bd855e45e50cc
oskosk <oskosk@users.noreply.github.com>
2022-02-18 15:54:20 +0000

    My Jetpack: Check if product is active before checking if it requires plan (#22980)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1865266830

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '80d07eba4e9b53001a0d1974d049f39e2361cd9e',
+            'reference' => 'ec3c1d152a892bbb5ae1d77fa85b5b780ad8d0e6',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => 'debae842d55e4e91e8d6c82756747256e6358e60',
+            'reference' => '33c07e3573f2bac1bb432cd624086b14ac34cc68',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
