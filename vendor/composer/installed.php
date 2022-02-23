9c6785f06b8befdef3558442ec2d1ee41fce8349
oskosk <oskosk@users.noreply.github.com>
2022-02-23 13:06:00 +0000

    My Jetpack: Add Extras interstitial page (#23051)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1887350036

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '0cd2c2a4f231bd8523e2f375e2b8aab800149b83',
+            'reference' => '89e6970a5d2ff8efe061c74d3e66e5d3d22e7700',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => '40081e2ca8e3aa5ba743f74a83cc7a821ad5ed48',
+            'reference' => 'cfda143dd48bfa9979c25abddb07cf05e8e4e0b3',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
