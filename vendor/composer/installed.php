ae092e0feee4c412fda850f723fbd0ae4b3f89b8
retrofox <retrofox@users.noreply.github.com>
2022-02-24 20:42:28 +0000

    My Jetpack: compress extras product image (#23099)
    * my-jetpack: compress extras product image
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1895213976

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'd53b49a1fde0f39790fd1785dc8a1185ea8aed7b',
+            'reference' => '363fcd238f4ed65dad6bd1a7f699aeff5ef072b8',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => 'b0090ebab6c65253d4eae1180257958413eb4b07',
+            'reference' => '15df8afb9a8d823b33e74f1ac528cb4558e4967b',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
