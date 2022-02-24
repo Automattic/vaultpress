d06f82e1a01f9b0e9ce5b67b1c2c4c32c6628a34
gavande1 <gavande1@users.noreply.github.com>
2022-02-24 13:51:59 +0000

    Add missing plugins sidebar menu for Atomic site with unsupported plan (#23086)
    * add missing plugins menu for free atomic site
    
    * add changelog
    
    * added better doc comments
    
    * Add better doc comment
    
    Co-authored-by: Miguel Torres <miguel.torres@automattic.com>
    
    * fix lint
    
    Co-authored-by: Miguel Torres <miguel.torres@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1893325626

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '1a01461d515b5a6728c99a4bc505e06015e81545',
+            'reference' => '7d5372518ce53567f1555669babac65085802727',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => 'a812c2898a933dc540745f9b64a41ff2f0a402fa',
+            'reference' => '43c9340e684f91fea64c29d0c5fc5001c9f564e0',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
