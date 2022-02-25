bbbc03606862f112e0b46dbacaad6554f48e0a4d
jeherve <jeherve@users.noreply.github.com>
2022-02-25 08:34:43 +0000

    Search: Fix regression preventing clicks from spawning overlay (#23102)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1897612162

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'e8e2bed21ed2812b1b717a2ee38d5a7c90f4307b',
+            'reference' => '8c0542eac2d53174b543bf6ffe572fb2aafb5b47',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => '3a474bd75788f5f5ab840f385865bd78b0bfeaff',
+            'reference' => '8db1d6be1bfc2c1e7a876fcd5e7e1bca0f26be98',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
