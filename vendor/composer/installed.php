b92f79ddbed63d699a99271392896d7fc483b410
anomiex <anomiex@users.noreply.github.com>
2022-02-28 17:26:01 +0000

    actions: Direct CI Slack notifications to different channels (#23143)
    We've decided to split the notifications, with master going to one
    channel and release branches going to another.
    
    There are two ways this could have been done: have a step determine the
    channel as an output to feed to the send-message step, or have
    send-message steps per channel with `if` used to enable the appropriate
    steps. I decided to go with the latter, since it's more straightforward
    that way if at some point in the future we decide messages might want to
    go to more than one channel (e.g. say Boost wanted notifications about
    their release branch to go to their channel too).
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1911603611

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '3edbe5f902f3b631a1c8875bc83ac271cdac609d',
+            'reference' => 'e40d07ba6c2ba84dd0acd886f2e878f6edfea94f',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => '121b677fa032628b636f94f3540f3f96ec8d7e72',
+            'reference' => 'a59dc93cc041c139ea9547cc39759a144a0fc510',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
