1d90ac3aa2c38c9f124e1d9e814e2a4e00bbd50b
jeherve <jeherve@users.noreply.github.com>
2022-03-01 07:08:09 +0000

    Search: Update Customberg class file name (#23158)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1914670973

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'acf4e8b7a6321d6f9e8f455ca6b735c84ecc134a',
+            'reference' => 'be3b90f44fa5d9e9a8b90fac1502461eff0bc347',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => '9f8605176698adc9397793eb6e889eb9c309e042',
+            'reference' => 'c72ce9229caaa9692ab4f8f0ba6c994adb15e163',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
