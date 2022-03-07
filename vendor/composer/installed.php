e431869fce09b47d83967c09a26d61a4e702a2b6
jeherve <jeherve@users.noreply.github.com>
2022-03-07 13:53:24 +0000

    VideoPress: Fix missing block attributes when using resumable uploads (#23226)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1945786561

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'bf6e7f899a8e4bf3c4f4cb995936252886ab11a1',
+            'reference' => '5b7db025ef2369cb50ecc794b514733ced7e829f',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => 'cb1821023cf6cd70b10148b79b3922044c8e34ad',
+            'reference' => '6762adaac577dd2705facefce2040464f62bf465',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
