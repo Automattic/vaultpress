2c211f03bdc03ce5a4ca662372f4fda0cf6f5be6
kraftbj <kraftbj@users.noreply.github.com>
2022-03-08 22:35:04 +0000

    PHPCS: Add WordPress ruleset (#23305)
    * Use the generic WordPress ruleset to match WP.com
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1954146868

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'ca9b547e01d33ff03a189404168c324975194d31',
+            'reference' => '45165bb671a68801355dd1dff2de5bd37848492d',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => '4f8a49afabbd71d1387c16a520810772dc5a1d5e',
+            'reference' => '70f7e283e7661b08eafad3aacb8cd9aa5b689b25',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
