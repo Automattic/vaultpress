a9a1912c50ed07d3dddd5275e915393a2f7f87c2
samiff <samiff@users.noreply.github.com>
2022-03-08 15:30:43 +0000

    Premium Content block: Don't allow block to be nested (#23220)
    * Don't allow a Premium Content block to be added inside of a different Premium content block. This restriction happens only for direct children of the block.
    
    However, a user can still add a premium content block if they use an intermediary block (e.g. Group). This behaviour was not restricted due to technical shortcomings of the current Gutenberg API.
    
    * Extract the select so that it can be reused both in subscriber and guest view.
    
    * Refactor the implementation to use a usePremiumContentAllowedBlocks function.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1952235852

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '70123dcb49ae517f97fa4df10595540717557db2',
+            'reference' => '2c6239f47e96fda6bb0fc64bfce8b6817ca314fd',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => '2e708797ca83e84892ecb56c33aebc8b4dc1a871',
+            'reference' => '5e285235583574e4552279e6e8d50f8d5056b957',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
