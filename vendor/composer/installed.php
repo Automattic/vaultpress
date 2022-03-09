91f8d94c6dd68bf3b1191b394bb138fbd9b102e8
oskosk <oskosk@users.noreply.github.com>
2022-03-09 13:23:30 +0000

    RNA: Add new values from `My Jetpack` at `ThemeProvider` (#23308)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1957425117

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'e4680817e7e8ed5b1eb24a7b1e6d05d1f80d4b60',
+            'reference' => 'f28684df6d9a09d622804e878f3ff4659499769f',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => 'b6dc1f89da9425754128a844fb41a45c7b5766d6',
+            'reference' => '70949437483952b9f8e1601cf7b7e9e15cfa10b0',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
