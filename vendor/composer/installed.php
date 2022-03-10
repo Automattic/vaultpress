63575900cd31c72316ecf5ac060dfae5f7b132de
kraftbj <kraftbj@users.noreply.github.com>
2022-03-10 21:37:19 +0000

    renovate: Run self-hosted via GitHub Actions (#23307)
    * renovate: Run self-hosted via GitHub Actions
    
    The switch to using `.pnpmfile.cjs` for overrides in #22965 turned out
    to make renovate unhappy, as they [intentionally don't use that file][1].
    
    The solution is to run renovate "self-hosted", which we can do easily
    enough via GitHub Actions. That also gives us a few other benefits:
    
    * No need for the "renovate helper" workflow anymore, as we can
      configure postUpgradeTasks with a script to do the needful.
    * No need to manually update the list of monorepo projects in
      renovate.json anymore, because we can use a .js config file for the
      self-hosted setup.
    * That also lets us use comments in the config file.
    
    The down side is that we're going to have to set up an account or app
    for renovate to run as with a token that has the "workflow" scope. The
    existing matticbot token doesn't have it.
    
    [1]: https://github.com/renovatebot/renovate/discussions/11250
    
    * Allow setting log level when manually running the workflow
    
    * Fix post-upgrade script
    
    The renovate container apparently lacks the tools we need. Run the stuff
    inside our jetpack docker env instead, since that already has the right
    versions of the tools.
    
    Then fix a few bugs in the script itself that turned up while testing.
    
    * Use new matticbot token
    
    * Adjust various checks for renovate PRs to recognize those created by matticbot too
    
    * Update cron, remove on-push trigger, and add a footer to the dashboard pointing out the run parameters and manual trigger location.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1965593073

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'aecf1857a345c430368300337ed2d92aeefbca87',
+            'reference' => '72fc43f711fdbf0e0cb2927d49a55d45e06a6aa2',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => '7abe49e5b20558ac583e73903569907f336e4f20',
+            'reference' => '368fd3e45baf715bcdf76c4d266298c248278310',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
