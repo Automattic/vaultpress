bbebb9a8c9fe197871ec59497e38e021b0030dd6
oskosk <oskosk@users.noreply.github.com>
2022-03-10 18:53:44 +0000

    Jetpack: Add QRPost code to Jetpack sidebar (#23336)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1964895977

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '15fddd37aceab9735bac2a7b963df48e637ead93',
+            'reference' => 'b6bd72f2b0cc6bbee1ad5685385cbddebfd2e283',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => '6f6792c2eea52f02878f4cba441596c9cf369e1a',
+            'reference' => 'fe3455b4d39914d631bd9ce8990a0a2b8f47f14e',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
