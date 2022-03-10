c7dc3043e381a6564c2e200ef9994ecf1ca2a4c4
bindlegirl <bindlegirl@users.noreply.github.com>
2022-03-10 17:54:17 +0000

    Admin pages: remove extra filtering (#23348)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1964633604

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '4459bd3be8ece672b775c29c43aeb7e8fc803675',
+            'reference' => 'd76f41e0051761ab78f4d2b900dd9f2e8bef3443',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => '9af0bb2bcc8a074359ad31b686781fae161f8496',
+            'reference' => '6acaba5c6e01d19b22cb036f899338bc2fbb18a2',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
