8e316ba0dcde35e4e9a723c730f7d429d2af38d2
oskosk <oskosk@users.noreply.github.com>
2022-03-10 12:33:13 +0000

    Jetpack: Add View and Download buttons to QRPost (#23304)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1963050379

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'c98265e79c878975f4461709f7a94045d8910a0f',
+            'reference' => '0ae46ef4e1ccf05579525c8da5b71b97cfd01d97',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => '83de2a267e1b5b93ac1cdd31a55e594c85d769fa',
+            'reference' => 'c0e67c7353baac9202bef2089ce04832c9bb445b',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
