b57f4c8e35c4648a0dea59da79f8271c011beea8
sdixon194 <sdixon194@users.noreply.github.com>
2022-03-10 19:30:34 +0000

    CLI: Let auto-changelogger work after commits (#23227)
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1965063608

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'b6bd72f2b0cc6bbee1ad5685385cbddebfd2e283',
+            'reference' => 'aecf1857a345c430368300337ed2d92aeefbca87',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => 'fe3455b4d39914d631bd9ce8990a0a2b8f47f14e',
+            'reference' => '7abe49e5b20558ac583e73903569907f336e4f20',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
