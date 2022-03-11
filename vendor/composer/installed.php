aef8795c8db3ebc4ee8c8da6cfd2371c29df84ff
anomiex <anomiex@users.noreply.github.com>
2022-03-11 14:56:51 +0000

    tools: Update exclusion in get-contributor-list.sh (#23377)
    Matticbot shows up as "Calypso Bot", not "matticbot".
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1969368567

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '3fade1e1db9092bee3765b9d417aedbce4286245',
+            'reference' => 'd984d84baf56c728d96d03093031cebfa00237e2',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => 'd263dfbba24065fa559d6a3413f39e29d1256b51',
+            'reference' => 'b160c4042ce92a4dd84efeb40b3620b64cad96de',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
