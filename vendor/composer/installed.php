357ef62b48d293dacf544d89faf8dfd40fa4dab3
anomiex <anomiex@users.noreply.github.com>
2022-03-11 13:45:20 +0000

    js-tools: Simplify eslint react configs (#23360)
    We can save a bit of effort in various projects' eslint configs if we
    let the react eslint config be used to extend the standard config
    instead of having to `root: true` it. And we can clean up the monorepo
    root dir slightly by moving the extendable eslint configs into
    `tools/js-tools/`.
    
    This also speeds up the linting slightly. Not a whole lot, maybe 5%.
    
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1969042446

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'a87a3684525b9e8e0fc5e3fef65c39339dc1c0cd',
+            'reference' => '6f9800797d76bb010bd073e4d3dcf16df9dbaabc',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => 'cd57f92f429a21a1b81861392fc1ef77a077d5e9',
+            'reference' => '6d794ba6790ede57265352b70d167e4d25795870',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
