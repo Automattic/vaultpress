63470cdfe81736275fed5d7cef0dfb48dc6f0f0a
kraftbj <kraftbj@users.noreply.github.com>
2022-03-14 21:43:19 +0000

    Google Fonts: update the method used to preconnect Fonts source domain. (#23322)
    * Google Fonts: update method used to preconnect Fonts source domain
    
    Follow-up to #23045
    
    Let's favor using a Core implementation with a Core filter instead of manually adding to the head.
    
    * Use filter instead of action
    
    * Only add resource hint if the site supports the Webfonts API
    
    See #23306
    
    * Update readme with latest method name.
    
    * Introduce new class that doesn't depend on WP_Webfonts_Provider
    
    WP_Webfonts_Provider may not always be available, so we cannot call a method from a child class of `WP_Webfonts_Provider` from a WP filter. Let's introduce a new class instead, that doesn't depend on anything, and do the class check inside there. That should simplify things a bit for the folks having to implement that filtering.
    
    Co-authored-by: Brandon Kraft <public@brandonkraft.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1983275068

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'cd93acc894829ab1f0ebaa23c4e9d1b7cf5329f6',
+            'reference' => '91ff4eb8262bcf8f4e8d428e6ef15b60f55cc98f',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => '993a403b1f5e692f5d32abca4751812e32cfde58',
+            'reference' => '8047f4488aac355b9c62776303a7aa79a2a7f1cf',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
