c293a0d14dfbdfcf7da5e08271d8d194c6f0ee58
leogermani <leogermani@users.noreply.github.com>
2022-03-17 20:59:24 +0000

    Changed page title (#23491)
    Co-authored-by: Brandon Kraft <public@brandonkraft.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2000796644

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'd5ee76c93786ce23b43db96b52f1c98a3fa90953',
+            'reference' => 'ba80eeb2fe3b8073c58afb1f4d71da511d275185',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => 'a0e95762b83cdef144983ac3152d40287a56e24d',
+            'reference' => 'b76a5e610e92f83e826ddb5ad58cbbc6d5934160',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
