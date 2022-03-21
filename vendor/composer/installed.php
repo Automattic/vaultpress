cd34d0cfcb87ab9cd19f3a50948ddaea8ad133c4
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2022-03-21 14:24:14 +0000

    Add tracking for backup storage info bar chart (#23461)
    * [not verified] Add tracking for the new bar chart component
    
    * [not verified] Fix imports
    
    * [not verified] Create add-at-a-glance-backup-storage-bar-chart-tracking
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2016626588

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'ce3a28f5dfb1efa692c600d4c7146010bf5849eb',
+            'reference' => '380c5516b4a337012b1fc3be9d948e390dfc40e9',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => '71a0684cd6ab69391ea930b8a2691672a9f97d3d',
+            'reference' => '99ebd558a39bf82542bceba3649b00905fb6afb0',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
