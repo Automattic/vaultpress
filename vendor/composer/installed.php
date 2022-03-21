00c613798088895ad7581722a3f9129266e8cf71
oskosk <oskosk@users.noreply.github.com>
2022-03-21 19:10:17 +0000

    My Jetpack: Update product icon opacity in Product cards (#23525)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2018163212

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '0565f448e81f1dd7ead3f9d0e0c0d3507d2827a8',
+            'reference' => '86120c9426850e8199529c8a84a0d45ba0179884',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => '8cf820cacaea9e8563b28f616a69e40a29b9067c',
+            'reference' => '23c5074b31abca9393f78445fabcdd7427b9ae80',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
