b4a8aac8ed0793c0d2cf4d0ee481e259671823dd
anomiex <anomiex@users.noreply.github.com>
2022-03-28 20:59:25 +0000

    build: Strip workspace refs from mirrors (#23654)
    They aren't going to be valid in the mirrors anyway, and when it comes
    to JS packages they might confuse things. Best to strip them out,
    leaving just the "public" version number.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2054819122

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '97324c48dcd917ecab0e927955ad69ca84255bcd',
+            'reference' => '8d9f27f4912863f9a448c89c5964f7a16549c4df',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => 'e674785a525d755a1410afa67efdb3d1249bc6b6',
+            'reference' => '762c2ed66e585df7c74d922aad4da5322982307c',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
