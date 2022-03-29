7b6f640a71f9fb2c8409da002c895dbd2394bef5
jeherve <jeherve@users.noreply.github.com>
2022-03-29 14:51:28 +0000

    Fusion comment bot: update working to avoid confusion (#23666)
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2059225589

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'eec449f8dcaa60fe70b76cd2ab71cbfde0fd3860',
+            'reference' => 'f71632c12d994522b1761e49949d279db4576414',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => 'fa1196e6bb7779efc2bea8aa9fa5dfca8e3a52f8',
+            'reference' => 'f98b62fd1f721dd00a171d6231819e3328396db0',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
