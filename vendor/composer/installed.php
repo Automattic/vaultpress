604507a833473259d47d96528b33a26a0a4c35b0
jeherve <jeherve@users.noreply.github.com>
2022-03-29 06:45:31 +0000

    Payments Block: Propagate shared component controls to children (#23567)
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2056831785

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '214a0b4222b2fa8fbcdd1495d482b5426fa6734c',
+            'reference' => '8adc6225b3b01831406a834781ff55f708517e67',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => '0ed78ff66fae5e1388fb36bf4c63f5087cb18924',
+            'reference' => 'ed9a8c75bf2a8d067ba867ae074df4f49d9b5080',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
