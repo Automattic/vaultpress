40157520b3406c4ca177fd0b6d11e7bbe08999d7
anomiex <anomiex@users.noreply.github.com>
2022-03-29 14:47:05 +0000

    pre-push hook: Don't fail if no tty is available (#23674)
    PR #23359 added code to have the pre-push hook run `jetpack changelog add`,
    but this code fails if the push is happening via an IDE where a tty is
    not available.
    
    The fix is in two parts: First, don't redirect stdin from /dev/tty in
    .husky/pre-push if it's going to fail. Second, don't try to run
    `jetpack` in pre-push-hook.js if stdin isn't a tty.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2059193193

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'c89d910fd3f0968bba102e7d56e3f8294a94f917',
+            'reference' => 'eec449f8dcaa60fe70b76cd2ab71cbfde0fd3860',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => 'b12df3a4c9d7823ad6926ae359389860e08f04a8',
+            'reference' => 'fa1196e6bb7779efc2bea8aa9fa5dfca8e3a52f8',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
