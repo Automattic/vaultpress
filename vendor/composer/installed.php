3730a60400b0f66ece9c9d923de9f54eebf122af
jeherve <jeherve@users.noreply.github.com>
2022-03-29 06:23:49 +0000

    Payments: add new Payments post publish panel (#23118)
    Co-authored-by: Jacopo Tomasone <Copons@users.noreply.github.com>
    Co-authored-by: Niranjan Uma Shankar <niranjan.u@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2056758211

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '20ba90e3937811dd5e6b290e56f47638d7113708',
+            'reference' => '173bf30e5421220efde9e753bc357c368ec83e14',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => 'ccaa0eb5be9dc9ec67248ac7774fdfc93bf4e3d9',
+            'reference' => 'a9fd274297abbb83b1cf5479ac1b43d8a742fc78',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
