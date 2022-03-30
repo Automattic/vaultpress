dbe459cfaafc075dbfbf919adfcbf0311cafcb26
adimoldovan <adimoldovan@users.noreply.github.com>
2022-03-30 13:02:18 +0000

    Bump e2e dependencies versions (#23698)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2064850872

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => 'c848aca9f400579b660cbb65ee364a8937fde33c',
+            'reference' => '71c23234259510a3c326b3d9645b6092145e28b3',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => 'e47e4b6016049f3518725477e948238ac6d1ff40',
+            'reference' => 'ac5a52a2dd2c1422a66c78f918d5efbcc17320fe',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
