1caf436f51d2bd4e9b4746bc70f8c3587e082d77
retrofox <retrofox@users.noreply.github.com>
2022-03-30 15:44:44 +0000

    My Jetpack: tweak connection card styles and wording (#23707)
    * [not verified] connection: reword status card description
    
    * [not verified] my-jetpack: update user connection error color
    
    * [not verified] copnnection: tweak connect-user button link styles
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2065807240

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '01858f393d1e7d8aa87ad23aca90a4e66b3a9aef',
+            'reference' => '74548a9a7a0fa14a8dfb3ded501499159d1e7f73',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => '10374d325e1ad06d04c7aab98ee19887e5e798e5',
+            'reference' => '6ecfda9e9b44665ebd9f109813c68b8ed91dd6c1',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
