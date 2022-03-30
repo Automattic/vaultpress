b6da3fbf3a8c7092ca5e0d3980e550f661e9a956
retrofox <retrofox@users.noreply.github.com>
2022-03-30 11:51:56 +0000

    JS Components: Add spacing props to Text component (#23682)
    * [not verified] js-components: introduce Text spacing props
    
    * [not verified] js-components: set heading margins using spacing props
    
    * [not verified] js-components: rename spacing with mt, mr, mb and ml
    
    * [not verified] js-components: add pt, pr, pb and pl props
    
    * [not verified] js-components: add m and o prosp to Text
    
    * [not verified] js-componets: add mx, my, px, py props to Text
    
    * [not verified] js-components: allow set zero to Text box-model props
    
    * [not verified] js-components: improve generating Text CSS classes
    
    * [not verified] js-components: add box-model story to Text
    
    * [not verified] js-components: extends spacing to 8 units
    
    * [not verified] changelog
    
    * [not verified] js-components: fix margin-bottom for H3
    
    * [not verified] js-components: improces Text story
    props to @renatoagds https://github.com/Automattic/jetpack/pull/23682#discussion_r837819175
    
    * [not verified] js-components: add comment
    
    * bump jetpack-components version
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2064499300

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '232db105c7e0f6a7de8df34fa4ffd267049b7ccc',
+            'reference' => 'c848aca9f400579b660cbb65ee364a8937fde33c',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => '8b30031c0796692e0fa69503e857ffe4959802d6',
+            'reference' => 'e47e4b6016049f3518725477e948238ac6d1ff40',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
