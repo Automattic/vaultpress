f3c8e82605c7a1f6997474ece908f60012efa1db
jeherve <jeherve@users.noreply.github.com>
2022-03-31 13:58:56 +0000

    PHPCS: solving PHPCS linting errors in lib files, part 3 (#23329)
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    Co-authored-by: Brandon Kraft <public@brandonkraft.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2071447688

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '472b69e21b5023a5922ea9947b363ab40f2d6610',
+            'reference' => 'fb6eb7e96ae2752ce0ae09c8974bc249cc409631',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => 'fc44b550dad34b22e712d5f1e168198eaad0306c',
+            'reference' => '7e5d2a07216a541cc3f71d102c0c3f78572e2c2b',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
