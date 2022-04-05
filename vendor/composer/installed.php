3b85f6220be441ea64564c22bdace2e07e523b00
jeherve <jeherve@users.noreply.github.com>
2022-04-05 05:27:13 +0000

    Settings: add a caution message for the Jetpack Blocks toggle (#23766)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2094158315

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '5d6a99a55432420a97b8929214968318798db42b',
+            'reference' => '12faa8c61319beabeb6893cc936f412c11321631',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => 'd7143aa862a7813c9de1d58dc0c4b9ed45beb2fa',
+            'reference' => 'b10f50c589b237e64532d0b108391d321a8fcff8',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
