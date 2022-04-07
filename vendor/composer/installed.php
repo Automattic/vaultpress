5a20c0b96ac6e6e934f5d06933ecc9a2c750aae2
renatoagds <renatoagds@users.noreply.github.com>
2022-04-07 21:36:05 +0000

    Protect: Add `title` and redirect at `VulnerabilitiesList` (#23827)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2111644185

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -16,7 +16,7 @@
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
-            'reference' => '8992981ebae607dee6f1dd955bf521d3ce5f9729',
+            'reference' => '5bc8d3413aa05ece00457e30ac7d6c6734e256c5',
             'dev_requirement' => false,
         ),
         'automattic/jetpack-logo' => array(
@@ -25,7 +25,7 @@
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
-            'reference' => '13947d4e90011147ec131f7daea0febc45c0cff2',
+            'reference' => '21593d694adf45c80e436e74ab6dc28e87d36805',
             'dev_requirement' => false,
         ),
         'automattic/vaultpress' => array(
