e351c6120a149a7bce71430f66d698d235c3403a
samiff <samiff@users.noreply.github.com>
2024-01-22 20:07:00 +0000

    Backport mu-wpcom-plugin 2.0.14, jetpack 13.1-a.3 Changes (#35170)
    * Changelog and readme.txt edits.
    
    * Init new cycle
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7616597850

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-autoloader' => array(
             'pretty_version' => '3.0.2',
             'version' => '3.0.2.0',
-            'reference' => '55587ee6e1ed4932df5fe0f642e853761e2fc0bb',
+            'reference' => '0f63274642965ebf688a4341673a6c2c268feb39',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'automattic/jetpack-logo' => array(
             'pretty_version' => '2.0.0',
             'version' => '2.0.0.0',
-            'reference' => '06e530062cc43f6c65d8f15bd43880d5f916921c',
+            'reference' => '5cbc54268f2d09caf7e2a70d0f3f479c40b7c0d7',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
