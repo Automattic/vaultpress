ec4ed7a64ea8bb9f6083fc6b26a6b3d6040b7748
zaerl <zaerl@users.noreply.github.com>
2024-03-13 07:03:26 +0000

    Changelog and readme.txt edits. (#36365)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8260626321

--- vendor/composer/jetpack_autoload_classmap.php
+++ vendor/composer/jetpack_autoload_classmap.php
@@ -19,7 +19,7 @@ return array(
 		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/class-autoloader-locator.php'
 	),
 	'Automattic\\Jetpack\\Assets\\Logo' => array(
-		'version' => '2.0.1.0-alpha1710273234',
+		'version' => '2.0.1.0',
 		'path'    => $vendorDir . '/automattic/jetpack-logo/src/class-logo.php'
 	),
 	'Automattic\\Jetpack\\Autoloader\\AutoloadFileWriter' => array(
