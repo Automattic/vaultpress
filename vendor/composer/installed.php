46cc9ac34ca5931bae91611fd404452f0cc0cc80
anomiex <anomiex@users.noreply.github.com>
2024-04-12 13:52:30 +0000

    phan: Allow deleting baseline files (#36857)
    In many cases it's pointless to keep around the empty baseline files,
    ideally they should never be repopulated. Update the tooling to allow
    for these to be deleted, in which case they won't be re-created by
    `--update-baseline`.
    
    But in case for some reason one does need to be re-created, add a
    `--force-update-baseline` option to allow for that.
    
    Also update the GitHub Action checking for baseline changes to handle
    these, and also to handle changes in pseudo-project baselines that
    wasn't done in #36467.
    
    For the moment I'm only actually removing the empty baselines for stuff
    Garage maintains. We can look at the rest in a followup.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8663003905

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -13,7 +13,7 @@
         'automattic/jetpack-autoloader' => array(
             'pretty_version' => '3.0.5',
             'version' => '3.0.5.0',
-            'reference' => 'f999a7e787007cbc49b221c58502dad7e79418a4',
+            'reference' => '490744c3bb92313eeecf872468afa9b7c1cf0dd3',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'automattic/jetpack-logo' => array(
             'pretty_version' => '2.0.2',
             'version' => '2.0.2.0',
-            'reference' => 'd0f6df33d4293c2344f68b7c13b9fdaf587d7b8b',
+            'reference' => '9bcc7448fa5871eb95f89eaa0b2229119b19ec10',
             'type' => 'jetpack-library',
             'install_path' => __DIR__ . '/../automattic/jetpack-logo',
             'aliases' => array(),
