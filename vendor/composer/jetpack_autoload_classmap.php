575c7a0c547fef0331085327a796aeb051892e67
anomiex <anomiex@users.noreply.github.com>
2025-03-03 18:33:16 +0000

    phpunit: Rename package test classes for PHPUnit 11 compat (pt 1) (#42120)
    PHPUnit 10+ requires that test class names match the filename, PSR-4
    style. This PR renames the test classes and files in packages to end in
    `Test` or `_Test`, depending on whether the class names are following
    WordPress's underscore-naming convention or are using CamelCase, and
    removing redundant `WP_Test_` or `Test_` prefixes.
    
    This also adds our standard Jetpack-Tests ruleset to
    tests/.phpcs.dir.xml in the packages, and removes some redundant inline
    phpcs:ignore or phpcs:disable directives.
    
    Of note, changed test order in the Connection package broke a test.
    Adding some setup/teardown code to clear the affected singleton fixed
    it.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13637614579
    
    Upstream-Ref: Automattic/jetpack@327f25f4acf9a71d70b3f6883f26acc3377365a5

--- vendor/composer/jetpack_autoload_classmap.php
+++ vendor/composer/jetpack_autoload_classmap.php
@@ -19,7 +19,7 @@ return array(
 		'path'    => $vendorDir . '/automattic/jetpack-autoloader/src/class-autoloader-locator.php'
 	),
 	'Automattic\\Jetpack\\Assets\\Logo' => array(
-		'version' => '3.0.1.0',
+		'version' => '3.0.2.0-alpha1741025873',
 		'path'    => $vendorDir . '/automattic/jetpack-logo/src/class-logo.php'
 	),
 	'Automattic\\Jetpack\\Autoloader\\AutoloadFileWriter' => array(
